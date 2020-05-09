727f362c7d3e8b7f7e8eb98c6f3177d68d0da4aa
tesao <shimeji9@gmail.com>
2020-05-09 16:39:45 +0900

    a-linkタグの追加

--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -1,12 +1,12 @@
 <template lang="html">
-<div class="">
+<!-- <div class="">
   <router-link to="Labyrinth">Go to labyrinth</router-link>
-</div>
+</div> -->
 <!-- VR空間内でのワールド移動を定義できるようにしたい・・・・ -->
-<!-- <a-scene>
+<a-scene>
   <a-sky color="#20b2aa"></a-sky>
-  <router-link id="to-labyrinth" tag="a-link" to="Labyrinth"></router-link>
-</a-scene> -->
+  <a-link href="/labyrinth" title="Go to Labyrinth!" position="0 1.3 -3"></a-link>
+</a-scene>
 </template>
 
 <script>
