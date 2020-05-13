197909cfec949dce6c38f399fdaed9fd8171a300
tesao <shimeji9@gmail.com>
2020-05-13 20:47:20 +0900

    aframe-extraから<a-ocean>を追加

--- src/vue-components/WarZone.vue
+++ src/vue-components/WarZone.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <a-scene physics="debug: true" vr-mode-ui="enabled: true">
+  <a-scene physics="debug: true">
     <!-- Assets -->
     <a-assets>
       <a-asset-item id="temple-obj" src="/assets/obj/temple-of-eshmoun/source/model.obj"></a-asset-item>
@@ -12,27 +12,21 @@
     <a-entity
       obj-model="obj: #temple-obj; mtl: #temple-mtl;"
       position="39.821 -16.500 -8.433"
-      static-body
     ></a-entity>
 
-    <a-entity
-      geometry="primitive: box;"
-      position="1.988 20 -10"
-      dynamic-body
-    ></a-entity>
+    <a-ocean position="35 -11.5 -37" scale="30 18 1"></a-ocean>
 
     <!-- Player Camera -->
     <a-entity
       id="rig"
       movement-controls="speed: 0.4;"
       position="1.988 1.535 -5.499"
-      dynamic-body
     >
       <a-entity
         camera
         player
         look-controls="pointerLockEnabled: true"
-        position="0 1.6 0"
+        position="0 3 0"
       ></a-entity>
       <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
       <a-entity id="rightHand" hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
