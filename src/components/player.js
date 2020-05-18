962b40e6e2b4824ac417b0701a066f7b7316d891
tesao <shimeji9@gmail.com>
2020-05-18 18:38:17 +0900

    環境音を追加

--- src/components/player.js
+++ src/components/player.js
@@ -10,15 +10,6 @@ AFRAME.registerComponent("player", {
     // this.el.setAttribute("direction", "0 0 -1")
   },
   tick: function() {
-    const el = this.el
-    this.el.addEventListener("raycaster-intersection", function () {
-      console.log("Player hit something!");
-      el.setAttribute("movement-controls", { enabled: false })
-    });
-    this.el.addEventListener("raycaster-intersection-cleared", function () {
-      console.log("cleared!");
-      el.setAttribute("movement-controls", { enabled: true })
-    });
   },
 
   update: function () {
--- src/vue-components/WarZone.vue
+++ src/vue-components/WarZone.vue
@@ -5,18 +5,36 @@
       <img id="sunset" src="/assets/sunset.jpg" />
       <a-asset-item id="temple-obj" src="/assets/obj/temple-of-eshmoun/source/model.obj"></a-asset-item>
       <a-asset-item id="temple-mtl" src="/assets/obj/temple-of-eshmoun/source/model.mtl"></a-asset-item>
-      <a-asset-item id="hand-obj" src="/assets/obj/hands/hand.OBJ"></a-asset-item>
-      <a-asset-item id="hand-mtl" src="/assets/obj/hands/Hand.mtl"></a-asset-item>
+      <audio id="world-battle-sound" src="/assets/sounds/world-sound.mp3" preload="auto" />
+      <audio id="running-tank" src="/assets/sounds/running-tank.mp3" preload="auto" />
+      <audio id="cannon-firing" src="/assets/sounds/cannon-firing.mp3" preload="auto" />
     </a-assets>
 
     <!-- War Zone Base Area -->
     <a-entity
       obj-model="obj: #temple-obj; mtl: #temple-mtl;"
       position="39.821 -16.500 -8.433"
+      sound="src: #world-battle-sound; loop: true; volume: 4; autoplay: true;"
+      sound__tank="src: #running-tank; loop: true; volume: 4; autoplay; true;"
+      sound__cannon="src: #cannon-firing; loop: true; volume: 4; autoplay; true;"
     ></a-entity>
 
     <!-- Sky -->
     <a-sky src="#sunset"></a-sky>
+
+    <!-- Player Camera -->
+    <a-entity
+      id="rig"
+      movement-controls="speed: 1;"
+      position="1.988 1.535 -5.499"
+    >
+      <a-entity
+        camera
+        player
+        look-controls="pointerLockEnabled: true"
+        position="0 3 0"
+      ></a-entity>
+    </a-entity>
 
     <!-- Tanks -->
     <!-- Elefant -->
@@ -127,23 +145,6 @@
       position="138.600 -4.440 -63.459"
       rotation="0 -164.655 0"
     ></a-entity>
-
-    <!-- Player Camera -->
-    <a-entity
-      id="rig"
-      movement-controls="speed: 0.4;"
-      position="1.988 1.535 -5.499"
-    >
-      <a-entity
-        camera
-        player
-        look-controls="pointerLockEnabled: true"
-        position="0 3 0"
-      ></a-entity>
-      <a-entity id="leftHand" hand-controls="hand: left; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
-      <a-entity id="rightHand" hand-controls="hand: right; handModelStyle: lowPoly; color: #ffcccc"></a-entity>
-    </a-entity>
-
   </a-scene>
 </template>
 
